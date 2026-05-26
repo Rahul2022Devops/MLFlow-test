bbcf4a74ebe863417ddb4d14209cd41884cb3233
Rahul Hasan <97163115+Rahul2022Devops@users.noreply.github.com>
2026-05-26 15:30:56 +0530

    basic calculator app code
    basic calculator app code

---
+++ calculator.py
@@ -0,0 +1,28 @@
+class Calculator:
+    def __init__(self):
+        self.num1 = int(input("Enter the first number"))
+        self.num2 = int(input("Enter the second number"))
+    def add(self):
+        r = self.num1 + self.num2
+        reasult = f"The sum of {self.num1} and {self.num2} is {r}"
+        print(reasult)
+    def substract(self):
+        r = self.num1 - self.num2
+        reasult = f"The substract of {self.num1} and {self.num2} is {r}"
+        print(reasult)
+    def multiply(self):
+        r = self.num1 * self.num2
+        reasult = f"The multiply of {self.num1} and {self.num2} is {r}"
+        print(reasult)
+    def divide(self):
+        r = self.num1 / self.num2
+        reasult = f"The divide of {self.num1} and {self.num2} is {r}"
+        print(reasult)
+
+if __name__ == "__main__":
+    cal = Calculator()
+    cal.add()
+    cal.substract()
+    cal.multiply()
+    cal.divide()
+
